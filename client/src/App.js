8a4271b465a82d05ab1c1a75e3e82a77c38a4312
EmL01 <contact@emlhd.com>
2021-07-09 14:42:22 +0200

    tailwindcss setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      Netflix Clone
-    </div>
-  );
+  return <div className="font-sans">Netflix Clone</div>;
 }
 
 export default App;
